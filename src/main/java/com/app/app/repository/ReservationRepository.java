62f514ce9956416b4c9888d59111fdd8e26f6b54
jxx <1440631515@qq.com>
2018-12-18 16:29:20 +0800

    reservation

--- src/main/java/com/app/app/repository/ReservationRepository.java
+++ src/main/java/com/app/app/repository/ReservationRepository.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 public interface ReservationRepository extends JpaRepository<Reservation,Integer> {
 
-     @Query(value = "SELECT s.title,s.expert_name,s.subscribe_address,s.text,r.state,s.subscribe_time ,e.`name`,e.position,e.school,e.specialty FROM  reservation r join service s JOIN expert_customize e WHERE r.service_id=s.id and r.wid=1 AND s.expert_name=e.`name`",
+     @Query(value = "SELECT s.title,s.expert_name,s.subscribe_address,s.text, r.state,s.subscribe_time ,e.position,e.school,e.specialty FROM  reservation r join service s JOIN expert_customize e WHERE r.service_id=s.id AND s.expert_name=e.`name` and r.wid=?1",
             nativeQuery = true)
      List<Object> getReservationLists (String wid);
 
